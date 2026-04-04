0c0af9cfb5ac918c47d3b27f78e3e5b47f4e86ce
Yurii <yuriydavydyak@gmail.com>
2026-04-04 19:29:38 +0300

    Homework 08_2 lesson 08 updated

--- lesson_08_2/homework_08_2.py
+++ lesson_08_2/homework_08_2.py
@@ -8,21 +8,18 @@ class Rhombus:
             if value <= 0:
                 raise ValueError("Side must be greater than 0")
 
-        if name == "angle_a":
+        elif name == "angle_a":
             if not (0 < value < 180):
                 raise ValueError("Angle must be between 0 and 180")
 
             super().__setattr__("angle_b", 180 - value)
 
+        elif name == "angle_b":
+            raise AttributeError("angle_b is calculated automatically and cannot be set directly")
+
         super().__setattr__(name, value)
 
 rhombus = Rhombus(5, 60)
 
-print(rhombus.side_a)  # 5
 print(rhombus.angle_a)  # 60
 print(rhombus.angle_b)  # 120
-
-rhombus.angle_a = 100
-
-print(rhombus.angle_a)  # 100
-print(rhombus.angle_b)  # 80
