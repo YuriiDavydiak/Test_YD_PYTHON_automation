3e6857ab65dcbcf4490ed075de2770816a10504e
Yurii <yuriydavydyak@gmail.com>
2026-04-13 11:10:07 +0300

    Homework 12 lesson 12

--- lesson_12/homework_12.py
+++ lesson_12/homework_12.py
@@ -3,84 +3,85 @@ from lesson_07.homework_07 import reverse_string, get_longest_word, find_substri
 from lesson_11.homework_11 import sum_numbers
 
 
-class TestHomeworks(TestCase):
+# reverse_string
+class TestReverseString(TestCase):
 
-    #  reverse_string
     def test_reverse_string_basic(self):
-        test_input = "hello"
-
-        actual_result = reverse_string(test_input)
-
+        actual_result = reverse_string("hello")
         assert actual_result == "olleh"
 
     def test_reverse_string_empty(self):
-        test_input = ""
-
-        actual_result = reverse_string(test_input)
-
+        actual_result = reverse_string("")
         assert actual_result == ""
 
-    #  get_longest_word
+    def test_reverse_string_with_numbers(self):
+        actual_result = reverse_string("12345")
+        assert actual_result == "54321"
+
+
+# get_longest_word
+class TestGetLongestWord(TestCase):
+
     def test_get_longest_word_basic(self):
-        test_input = ["a", "abc", "ab"]
-
-        actual_result = get_longest_word(test_input)
-
+        actual_result = get_longest_word(["a", "abc", "ab"])
         assert actual_result == "abc"
 
-    def test_get_longest_word_empty(self):
-        test_input = []
 
+    def test_get_longest_word_same_length(self):
+        actual_result = get_longest_word(["aa", "bb", "cc"])
+        assert actual_result in ["aa", "bb", "cc"]
+
+
+    def test_get_longest_word_invalid_type(self):
+        test_input = "not a list"
         actual_result = get_longest_word(test_input)
+        assert actual_result in test_input
 
-        assert actual_result == ""
 
-    #  sum_numbers
+# sum_numbers
+class TestSumNumbers(TestCase):
+
     def test_sum_numbers_valid(self):
-        test_input = "1,2,3"
-
-        actual_result = sum_numbers(test_input)
-
+        actual_result = sum_numbers("1,2,3")
         assert actual_result == 6
 
-    def test_sum_numbers_invalid(self):
-        test_input = "1,a,3"
-
-        actual_result = sum_numbers(test_input)
-
+    def test_sum_numbers_invalid_data(self):
+        actual_result = sum_numbers("1,a,3")
         assert actual_result == "Не можу це зробити!"
 
     def test_sum_numbers_single(self):
-        test_input = "5"
-
-        actual_result = sum_numbers(test_input)
-
+        actual_result = sum_numbers("5")
         assert actual_result == 5
 
-    #  find_substring
+
+    def test_sum_numbers_empty_string(self):
+        actual_result = sum_numbers("")
+        assert actual_result == "Не можу це зробити!"
+
+
+# find_substring
+class TestFindSubstring(TestCase):
+
     def test_find_substring_found(self):
-        str1 = "hello world"
-        str2 = "world"
-
-        actual_result = find_substring(str1, str2)
-
+        actual_result = find_substring("hello world", "world")
         assert actual_result == 6
 
     def test_find_substring_not_found(self):
-        str1 = "hello"
-        str2 = "xyz"
-
-        actual_result = find_substring(str1, str2)
-
+        actual_result = find_substring("hello", "xyz")
         assert actual_result == -1
 
     def test_find_substring_empty(self):
-        str1 = ""
-        str2 = "a"
-
-        actual_result = find_substring(str1, str2)
-
+        actual_result = find_substring("", "a")
         assert actual_result == -1
+
+
+    def test_find_substring_invalid_type(self):
+        try:
+            find_substring(123, "a")
+        except Exception:
+            assert True
+        else:
+            assert False
 
 
 if __name__ == "__main__":
