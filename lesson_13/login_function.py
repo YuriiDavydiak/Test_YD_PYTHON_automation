0b634d7745704f340a350cdc279621c075986798
Yurii <yuriydavydyak@gmail.com>
2026-04-20 18:04:58 +0300

    Homework 13 lesson 13 updated

--- lesson_13/login_function.py
+++ lesson_13/login_function.py
@@ -1,35 +1,16 @@
 import logging
 
 
-def get_logger():
-    logger = logging.getLogger("log_event")
-
-    if not logger.handlers:
-        logger.setLevel(logging.INFO)
-
-        file_handler = logging.FileHandler("login_system.log")
-        file_handler.setLevel(logging.INFO)
-
-        console_handler = logging.StreamHandler()
-        console_handler.setLevel(logging.INFO)
-
-        formatter = logging.Formatter(
-            "%(asctime)s - %(levelname)s - %(message)s"
-        )
-
-        file_handler.setFormatter(formatter)
-        console_handler.setFormatter(formatter)
-
-        logger.addHandler(file_handler)
-        logger.addHandler(console_handler)
-
-    return logger
-
-
 def log_event(username: str, status: str):
-    logger = get_logger()
-
     log_message = f"Login event - Username: {username}, Status: {status}"
+
+    logging.basicConfig(
+        filename='login_system.log',
+        level=logging.INFO,
+        format='%(asctime)s - %(message)s'
+    )
+
+    logger = logging.getLogger("log_event")
 
     if status == "success":
         logger.info(log_message)
