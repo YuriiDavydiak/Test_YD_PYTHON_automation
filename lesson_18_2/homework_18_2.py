ee4470b09ce965f5734698e6f29f91ff52cfa30c
Yurii <yuriydavydyak@gmail.com>
2026-05-10 22:40:22 +0300

    Homework 18_2 lesson 18

---
+++ lesson_18_2/homework_18_2.py
@@ -0,0 +1,43 @@
+import requests
+from urllib.parse import quote
+
+BASE_URL = "http://127.0.0.1:8080"
+IMAGE_PATH = "mars_photo1.jpg"
+
+def upload_image(image_path):
+    url = f"{BASE_URL}/upload"
+    with open(image_path, "rb") as img:
+        files = {"image": img}
+        response = requests.post(url, files=files)
+    response.raise_for_status()
+    data = response.json()
+    print(f"Завантажено. Відповідь: {data}")
+    return data["image_url"]
+
+def get_image_url(filename):
+    encoded = quote(filename)
+    url = f"{BASE_URL}/image/{encoded}"
+    headers = {"Content-Type": "text"}
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    data = response.json()
+    print(f"Отримано URL: {data}")
+    return data["image_url"]
+
+def delete_image(filename):
+    encoded = quote(filename)
+    url = f"{BASE_URL}/delete/{encoded}"
+    response = requests.delete(url)
+    response.raise_for_status()
+    data = response.json()
+    print(f"Видалено. Відповідь: {data}")
+
+if __name__ == "__main__":
+
+    image_url = upload_image(IMAGE_PATH)
+
+    filename = image_url.split("/uploads/")[-1]
+
+    get_image_url(filename)
+
+    delete_image(filename)
